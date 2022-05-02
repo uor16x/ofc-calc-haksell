faf46f75bf615458bd5304f755e860d85b21ffb0
uor16x <u0r.home@gmail.com>
2022-05-02 21:28:41 +0300

    Parse suit;

--- calc.hs
+++ calc.hs
@@ -1,3 +1,5 @@
+import Data.List (elemIndex)
+import Data.Char (toLower)
 data Value = Two
     | Three
     | Four
@@ -16,3 +18,17 @@ data Suit = Hearts
     | Diamonds
     | Clubs
     | Spades deriving (Eq, Show, Enum, Bounded)
+
+data Card = Card {
+    value :: Value,
+    suit :: Suit
+}
+
+parseSuit :: Char -> Maybe Suit
+parseSuit symbol = case suitIndex of
+    Nothing -> Nothing
+    Just index -> Just (toEnum index :: Suit)
+    where
+        allSuits = [minBound .. maxBound] :: [Suit]
+        suitSymbols = [toLower . head . show $ s | s <- allSuits]
+        suitIndex = symbol `elemIndex` suitSymbols
