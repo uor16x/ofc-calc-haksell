b5f079e88d941f1f37bf28e4a9bfda1a634023ec
uor16x <u0r.home@gmail.com>
2022-05-03 16:45:04 +0300

    parseCard method;

--- src/CardParts/Cards.hs
+++ src/CardParts/Cards.hs
@@ -1,11 +1,35 @@
 -- | Combined (value + suit) card data type and its processing methods
 module CardParts.Cards where
 
-import CardParts.Suits ( Suit, parseSuit )
-import CardParts.Values ( Value, parseValue )
+import CardParts.Suits ( Suit (..), parseSuit )
+import CardParts.Values ( Value (..), parseValue )
 
 -- | This type represents a card - combination of suit and value.
 data Card = Card {
     value :: Value,
     suit :: Suit
 }
+
+{- | This method gets a string which represents full card combination (value + suit)
+and returns a 'Card' wrapped with 'Maybe'. The function uses 'parseValue' and 'parseSuit' methods.
+
+Passed string should have length 2 and have format "{VALUE}{SUIT}".
+
+__Examples:__
+
+@
+parseCard "2c" = 'Just' 'Two' 'Clubs'
+parseCard "As" = 'Just' 'Ace' 'Spades'
+parseCard "22c" = 'Nothing'
+parseCard "5f" = 'Nothing'
+parseCard "2x" = 'Nothing'
+@
+-}
+parseCard :: String -> Maybe Card
+parseCard [] = Nothing
+parseCard full
+    | length full /= 2 = Nothing
+    | otherwise = do
+        value <- parseValue $ head full
+        suit <- parseSuit $ last full
+        return $ Card { value = value, suit = suit }
