c15a5d3a546d4b27aa4a7c59630f375a02685374
uor16x <u0r.home@gmail.com>
2022-05-05 00:59:38 +0300

    Suit tests: Maybe -> Either;

--- test/SuitTest.hs
+++ test/SuitTest.hs
@@ -7,15 +7,15 @@ import Test.HUnit
       Counts,
       Test(TestLabel, TestCase) )
 
-testData :: [([Char], Char, Maybe Suit)]
+testData :: [([Char], Char, Either String Suit)]
 testData = [
-        ("Should return hearts", 'h', Just Hearts),
-        ("Should return diamonds", 'd', Just Diamonds),
-        ("Should return clubs", 'c', Just Clubs),
-        ("Should return spades", 's', Just Spades),
-        ("Should return nothing #1", 'q', Nothing),
-        ("Should return nothing #2", '1', Nothing),
-        ("Should return nothing #3", '-', Nothing)
+        ("Should return hearts", 'h', Right Hearts),
+        ("Should return diamonds", 'd', Right Diamonds),
+        ("Should return clubs", 'c', Right Clubs),
+        ("Should return spades", 's', Right Spades),
+        ("Should return nothing #1", 'q', Left "There is no card suit marked as 'q'"),
+        ("Should return nothing #2", '1', Left "There is no card suit marked as '1'"),
+        ("Should return nothing #3", '-', Left "There is no card suit marked as '-'")
     ]
 
 generateTests :: String -> [Test]
