27dc3c3c2aca85a66b9d580281a8fc35b4158d8c
uor16x <u0r.home@gmail.com>
2022-05-05 00:55:16 +0300

    Value tests Maybe -> Either;

--- test/ValueTest.hs
+++ test/ValueTest.hs
@@ -7,25 +7,25 @@ import Test.HUnit
       Counts,
       Test(TestLabel, TestCase) )
 
-testData :: [([Char], Char, Maybe Value)]
+testData :: [([Char], Char, Either String Value)]
 testData = [ 
-    ( "Should return 2", '2', Just Two ),
-    ( "Should return 3", '3', Just Three ),
-    ( "Should return 4", '4', Just Four ),
-    ( "Should return 5", '5', Just Five ),
-    ( "Should return 6", '6', Just Six ),
-    ( "Should return 7", '7', Just Seven ),
-    ( "Should return 8", '8', Just Eight ),
-    ( "Should return 9", '9', Just Nine ),
-    ( "Should return T", 'T', Just Ten ),
-    ( "Should return J", 'J', Just Jack ),
-    ( "Should return Q", 'Q', Just Queen ),
-    ( "Should return K", 'K', Just King ),
-    ( "Should return A", 'A', Just Ace ),
-    ( "Should return Nothing #1", '1', Nothing ),
-    ( "Should return Nothing #2", '-', Nothing ),
-    ( "Should return Nothing #3", 'z', Nothing ),
-    ( "Should return Nothing #4", 'k',Nothing )
+    ( "Should return 2", '2', Right Two ),
+    ( "Should return 3", '3', Right Three ),
+    ( "Should return 4", '4', Right Four ),
+    ( "Should return 5", '5', Right Five ),
+    ( "Should return 6", '6', Right Six ),
+    ( "Should return 7", '7', Right Seven ),
+    ( "Should return 8", '8', Right Eight ),
+    ( "Should return 9", '9', Right Nine ),
+    ( "Should return T", 'T', Right Ten ),
+    ( "Should return J", 'J', Right Jack ),
+    ( "Should return Q", 'Q', Right Queen ),
+    ( "Should return K", 'K', Right King ),
+    ( "Should return A", 'A', Right Ace ),
+    ( "Should return Nothing #1", '1', Left "There is no number card with value '1'"),
+    ( "Should return Nothing #2", '-',Left "There is no broadway card, which could be represented with '-'" ),
+    ( "Should return Nothing #3", 'z', Left "There is no broadway card, which could be represented with 'z'" ),
+    ( "Should return Nothing #4", 'k', Left "There is no broadway card, which could be represented with 'k'" )
     ]
 
 -- TODO: generalize this method
