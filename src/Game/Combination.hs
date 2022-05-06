3491afb8e49b06ece15b4273e58e8a98ed965359
uor16x <u0r.home@gmail.com>
2022-05-06 23:29:27 +0300

    Combinations parse: get occs etc. [WIP];

--- src/Game/Combination.hs
+++ src/Game/Combination.hs
@@ -1,7 +1,10 @@
 -- | Poker combination and its processing methods
 module Game.Combination(Combination(..), CombinationName(..)) where
 
-import CardParts.Cards (Card)
+import CardParts.Cards (Card (..))
+import CardParts.Values (Value(..))
+import CardParts.Suits (Suit(..))
+import Data.List ((\\))
 
 -- | Names of combinations enum
 data CombinationName = Kicker
@@ -44,3 +47,43 @@ instance Ord Combination where
         | part1 c1 == part1 c2 = part2 c1 `compare` part2 c2
         | otherwise = part1 c1 `compare` part1 c2
     c1 `compare` c2 = name c1 `compare` name c2
+
+parseCombination :: [Card] -> Combination
+parseCombination cards
+    | null pairs = RankCombination { name = RoyalFlush, rank = Card Ace Spades }
+    | otherwise = RankCombination { name = RoyalFlush, rank = Card Ace Spades }
+        where
+            pairs = [ occ | occ <- getOccurrences cards, count > 1]
+
+type OccurrencesCounter = [(Card, Int)]
+
+{- | This function gets a list of cards and returns a list of tuples.
+First element of which contains certain card, and second element contains number of occurences
+
+__Examples:__
+
+@
+getOccurrences [
+    'Card' {value = 'Ace', suit = 'Spades'},
+    'Card' {value = 'Jack', suit = 'Clubs'},
+    'Card' {value = 'Ace', suit = 'Hearts}
+] = [(Card {value = Ace, suit = Spades},2),(Card {value = Jack, suit = Clubs},1)]
+@
+-}
+getOccurrences :: [Card] -> OccurrencesCounter
+getOccurrences [] = []
+getOccurrences (x:xs) = (x, length xSameValue + 1) : getOccurrences(xs \\ xSameValue)
+    where
+        xSameValue = filter (\y -> value x == value y) xs
+
+parsePairHand :: [Card] -> OccurrencesCounter -> Either String Combination
+parsePairHand cards pairs = case length cards of
+    1 -> singlePairHand
+    2 -> doublePairsHand
+    n -> Left $ "Invalid number of pairs: " ++ show n
+    where
+        singlePairHand :: Either String Combination
+        singlePairHand = Left ""
+
+        doublePairsHand :: Either String Combination
+        doublePairsHand = Left ""
