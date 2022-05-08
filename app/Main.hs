c5b529a0efdf0396f8ef1ba1ae2824210c403904
uor16x <u0r.home@gmail.com>
2022-05-08 19:14:31 +0300

    parseBoard function;

--- app/Main.hs
+++ app/Main.hs
@@ -28,10 +28,21 @@ parseInput :: String -> [Input]
 parseInput args = map (uncurry InitialInput) (read args :: [(String, [String])])
 
 parseInputCards :: Input -> Either String Input
-parseInputCards InitialInput { username = username, strs = strs } =  case getUserBoard strs of
+parseInputCards InitialInput { username = username, strs = strs } = case getUserBoard strs of
     Right board -> Right BoardInput {
         username = username,
         board = board
     }
     Left msg -> Left $ "Parsing of input cards failed: " ++ msg
 parseInputCards _ = Left "Valid InitialInput should be passed here"
+
+parseBoard :: Input -> Either String Input
+parseBoard BoardInput { username = username, board = board } = case parsedCombinations of
+    Right result -> Right $ CombinationInput {
+        username = username,
+        combinations = result
+    }
+    Left msg -> Left $ "Parsing of combinations failed: " ++ msg
+    where
+        parsedCombinations = mapM parseCombination board
+parseBoard _ = Left "Valid BoardInput should be passed here"
